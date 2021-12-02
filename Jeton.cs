6a612a2d9bf57bdedc4c84853a7c03bd234817cd
Leonard Roussard <51063205+Lionvsx@users.noreply.github.com>
2021-12-02 18:38:58 +0100

    Initial Commit

---
+++ Jeton.cs
@@ -0,0 +1,7 @@
+namespace TD_Scrabble
+{
+    public class Jeton
+    {
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TD_Scrabble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
